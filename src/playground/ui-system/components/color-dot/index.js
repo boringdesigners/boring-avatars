6a6dca1f65adcc318a48225079d37173d1a47e64
Hayk An <say@hihayk.com>
2020-12-24 01:14:48 +0100

    color picker

--- src/playground/ui-system/components/color-dot/index.js
+++ src/playground/ui-system/components/color-dot/index.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { ChromePicker } from 'react-color'
+
+const DotWrapper = styled.div`
+  position: relative;
+`
 
 const Wrapper = styled.div`
   background-color: ${p => p.color};
   width: 2rem;
   height: 2rem;
   border-radius: 2rem;
+  cursor: pointer;
+`
+
+const PickerWrapper = styled.div`
+  position: absolute;
+  top: 2rem;
 `
 
 const ColorDot = ({ color }) => {
+  const [pickerIsOpen, setPickerIsOpen] = useState(false)
+  const [selectedColor, setSelectedColor] = useState(color)
+
   return (
-    <>
-      <Wrapper color={color} />
-      {/* <ChromePicker /> */}
-    </>
+    <DotWrapper>
+      <Wrapper
+        color={selectedColor}
+        onClick={() => setPickerIsOpen(!pickerIsOpen)}
+      />
+      {pickerIsOpen && (
+        <PickerWrapper>
+          <ChromePicker color={selectedColor} onChange={(value) => setSelectedColor(value.hex)} />
+        </PickerWrapper>
+      )}
+    </DotWrapper>
   )
 }
 
