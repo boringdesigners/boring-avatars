49d3b1c6f5b9e5e00e4c9f2ded13295bb9da25ae
Josep Martins <jsp.mrtns@gmail.com>
2020-12-27 20:29:08 +0100

    New Avatar texture

--- src/avatar/components/avatar-texture.jsx
+++ src/avatar/components/avatar-texture.jsx
@@ -6,35 +6,59 @@ function getRandomColor(colors, range) {
 
 function AvatarTexture(props) {
   const fill = getRandomColor(props.colors, props.colors.length)
+  const fill2 = getRandomColor(props.colors, props.colors.length)
+
 
   return (
     <div style={{display: 'inline-block', width: props.size, height: props.size}}>
       <svg
-        viewBox="0 0 80 80"
+        viewBox="0 0 140 140"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <mask
-          id="mask__a"
-          maskUnits="userSpaceOnUse"
-          x={0}
-          y={0}
-          width={80}
-          height={80}
-        >
-          <rect width={80} height={80} rx={40} fill="#fff" />
-        </mask>
+        <defs>
+          <pattern
+            id="prefix__c"
+            patternUnits="userSpaceOnUse"
+            width={10}
+            height={10}
+          >
+            <circle
+              cx={5}
+              cy={5}
+              r={4}
+              fill={fill}
+              stroke={fill}
+              strokeWidth={0}
+            />
+          </pattern>
 
-        <filter id="filter__a">
-          <feTurbulence baseFrequency={5} numOctaves={50} type="turbulence" result="turbulence" />
-          <feDisplacementMap
-            in2="turbulence"
-            in="SourceGraphic"
-            scale={200}
-            xChannelSelector="R"
-            yChannelSelector="G"
-          />
-        </filter>
-        <circle cx={0} cy={0} r={80} fill={fill} mask="url(#mask__a)" filter="url(#filter__a)" />
+          <pattern
+            id="prefix__b"
+            patternUnits="userSpaceOnUse"
+            width={4}
+            height={4}
+          >
+            <path
+              d="M0 4l4-4m-5 1l2-2m2 6l2-2"
+              stroke={fill}
+              strokeLinecap="square"
+            />
+          </pattern>
+          <pattern
+            id="prefix__a"
+            patternUnits="userSpaceOnUse"
+            width={60}
+            height={60}
+          >
+            <path
+              d="M0 60L60 0m-75 15l30-30m30 90l30-30"
+              strokeWidth={40}
+              //stroke={fill}
+              //strokeLinecap="square"
+            />
+          </pattern>
+        </defs>
+        <circle cx={70} cy={70} r={70} fill={fill} fill="url(#prefix__c)" />
       </svg>
     </div>
   )
