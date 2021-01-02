12eca5b68afbc6d8bcb383403a2c47192dec918e
Hayk An <say@hihayk.com>
2021-01-02 23:01:18 +0100

    lib set up

--- src/avatar/index.js
+++
@@ -1,1 +0,0 @@
-export { default as Avatar } from './components/avatar'
--- src/playground/ui-system/index.js
+++
@@ -1,5 +0,0 @@
-export { default as BaseStyles } from './components/base-styles'
-export { default as Button } from './components/button'
-export { default as Segment } from './components/segment'
-export { SegmentGroup } from './components/segment'
-export { default as ColorDot } from './components/color-dot'
