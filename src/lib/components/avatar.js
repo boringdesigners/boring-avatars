5f7c27e226b21e8516bf1c250a598190558b496f
Hayk An <say@hihayk.com>
2021-05-24 22:42:50 +0200

    cleanup

--- src/lib/components/avatar.js
+++ src/lib/components/avatar.js
@@ -8,15 +8,22 @@ import AvatarBeam from './avatar-beam'
 import AvatarSunset from './avatar-sunset'
 import AvatarMarble from './avatar-marble'
 
-const Avatar = ({ variant = 'marble', colors = ['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90'], name = 'Clara Barton', size = 40, ...props }) => {
+const Avatar = ({
+  variant = 'marble',
+  colors = ['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90'],
+  name = 'Clara Barton',
+  size = 40,
+  ...props
+}) => {
+  const avatarProps = {colors, name, size, ...props}
   const avatars = {
-    pixel: <AvatarPixel colors={colors} name={name} size={size} {...props}/>,
-    bauhaus: <AvatarBauhaus colors={colors} name={name} size={size} {...props}/>,
-    ring: <AvatarRing colors={colors} name={name} size={size} {...props}/>,
-    beam: <AvatarBeam colors={colors} name={name} size={size} {...props}/>,
-    sunset: <AvatarSunset colors={colors} name={name} size={size} {...props}/>,
-    dome: <AvatarDome colors={colors} name={name} size={size} {...props}/>,
-    marble: <AvatarMarble colors={colors} name={name} size={size} {...props}/>,
+    pixel: <AvatarPixel {...avatarProps}/>,
+    bauhaus: <AvatarBauhaus {...avatarProps}/>,
+    ring: <AvatarRing {...avatarProps}/>,
+    beam: <AvatarBeam {...avatarProps}/>,
+    sunset: <AvatarSunset {...avatarProps}/>,
+    dome: <AvatarDome {...avatarProps}/>,
+    marble: <AvatarMarble {...avatarProps}/>,
   }
   return avatars[variant]
 }
