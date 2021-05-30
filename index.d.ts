da1951679fef2977d6a38ad6898c519e4d3872bc
bozdoz <howaboutben+github@gmail.com>
2021-05-30 03:18:27 -0300

    Adds TypeScript Types (#3)
    * Adds initial typescript types
    
    * Adds "types" to package.json for TypeScript types
    
    * exports AvatarProps
    
    Co-authored-by: Jan Monschke <janmonschke@fastmail.com>
    
    Co-authored-by: Jan Monschke <janmonschke@fastmail.com>

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+declare module "boring-avatars" {
+  export interface AvatarProps {
+    size?: number | string;
+    name?: string;
+    variant?: "marble" | "beam" | "pixel" | "sunset" | "ring" | "bauhaus";
+    colors?: string[];
+  }
+
+  interface AvatarComponent {
+    (props: AvatarProps, context?: any): React.ReactElement | null;
+  }
+
+  const Avatar: AvatarComponent
+
+  export default Avatar;
+}
