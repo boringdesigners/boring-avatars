e030227efa8f19a8320b90c51953b7c66f88c1b4
Emerson Thompson <thompson.emerson@gmail.com>
2022-07-24 12:03:55 -0300

    simple improvements ;)

--- src/lib/utilities.js
+++ src/lib/utilities.js
@@ -1,9 +1,9 @@
 export const hashCode = (name) => {
-  var hash = 0;
-  for (var i = 0; i < name.length; i++) {
-      var character = name.charCodeAt(i);
-      hash = ((hash<<5)-hash)+character;
-      hash = hash & hash; // Convert to 32bit integer
+  let hash = 0;
+  for (let i = 0; i < name.length; i++) {
+    let character = name.charCodeAt(i);
+    hash = ((hash<<5)-hash)+character;
+    hash = hash & hash; // Convert to 32bit integer
   }
   return Math.abs(hash);
 }
@@ -17,7 +17,7 @@ export const getDigit = (number, ntn) => {
 }
 
 export const getBoolean = (number, ntn) => {
-  return (!((getDigit(number, ntn)) % 2))
+  return (!((getDigit(number, ntn)) % 2));
 }
 
 export const getAngle = (x, y) => {
@@ -25,11 +25,11 @@ export const getAngle = (x, y) => {
 }
 
 export const getUnit = (number, range, index) => {
-  let value = number % range
+  let value = number % range;
 
   if(index && ((getDigit(number, index) % 2) === 0)) {
-    return -value
-  } else return value
+    return -value;
+  } else return value;
 }
 
 export const getRandomColor = (number, colors, range) => {
@@ -37,21 +37,19 @@ export const getRandomColor = (number, colors, range) => {
 }
 
 export const getContrast = (hexcolor) => {
+  // If a leading # is provided, remove it
+  if (hexcolor.slice(0, 1) === '#') {
+    hexcolor = hexcolor.slice(1);
+  }
 
-	// If a leading # is provided, remove it
-	if (hexcolor.slice(0, 1) === '#') {
-		hexcolor = hexcolor.slice(1);
-	}
+  // Convert to RGB value
+  const r = parseInt(hexcolor.substr(0,2),16);
+  const g = parseInt(hexcolor.substr(2,2),16);
+  const b = parseInt(hexcolor.substr(4,2),16);
 
-	// Convert to RGB value
-	var r = parseInt(hexcolor.substr(0,2),16);
-	var g = parseInt(hexcolor.substr(2,2),16);
-	var b = parseInt(hexcolor.substr(4,2),16);
+  // Get YIQ ratio
+  const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
 
-	// Get YIQ ratio
-	var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
-
-	// Check contrast
-	return (yiq >= 128) ? '#000000' : '#FFFFFF';
-
+  // Check contrast
+  return (yiq >= 128) ? '#000000' : '#FFFFFF';
 };
