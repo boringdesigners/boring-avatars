c58d142126b268d0f00b35fc75add242346aa052
Enric Pallerols <enric@pallerols.cat>
2025-07-27 20:15:43 +0200

    fix vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts({ tsconfigPath: './tsconfig.lib.json', include: ['src/lib'] })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.tsx'),
+      fileName: 'index',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    copyPublicDir: false,
+    outDir: 'dist',
+  },
+});
