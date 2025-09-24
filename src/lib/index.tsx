eb160bf7d8eb51f9fe0c28e1a4ae002c1d4eb5c7
Enric Pallerols <enric@pallerols.cat>
2025-09-24 09:47:09 +0200

    respect defaults and fix types

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -22,7 +22,7 @@ const Avatar = ({
   colors = ['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90'],
   name = 'Clara Barton',
   title = false,
-  size,
+  size = '40px',
   square = false,
   ...otherProps
 }: AvatarProps & { variant?: keyof typeof AVATAR_VARIANTS; }) => {
